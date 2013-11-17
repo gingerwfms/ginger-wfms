809a5fc8ebbd55d6696ec49a66309d2ed67220a7
Codeliner <kontakt@codeliner.ws>
2013-11-17 21:52:13 +0100

    Setup application module

---
+++ module/Ginger/Application/Module.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the codeliner/ginger-wfms package.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Ginger\Application;
+
+/**
+ * Module class for Ginger\Application
+ * 
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        //autoloading is defined in the composer.json file
+        return array();
+    }
+}
